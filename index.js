0ac3fe3eaf39ed5461cbedeb7749c7b1a32348c0
João Azevedo <jazevedo@veniam.com>
2017-03-18 02:07:12 +0800

    manage child processes

--- index.js
+++ index.js
@@ -9,36 +9,41 @@ var mongo = require('mongodb').MongoClient;
 /********************************************************/
 
 var app = express();
-var fork_fbm = require('child_process').fork;
+var fork = require('child_process').fork;
+var child = [];
 var db;
 
 process.on('uncaughtException', function(err) {
     console.log('app.uncaughtException()', 'err', true, true, null, err.stack || err.message);
 });
 
+process.on('exit', function() {
+    console.log('killing', child.length, 'child processes');
+    child.forEach(function(process) {
+        process.kill();
+    });
+});
+
 app.use(parser.json());
 app.disable('x-powered-by');
 app.disable('etag');
 
-mongo.connect('mongodb://' + conf.mongo.user + ':' + conf.mongo.pwd + '@'+
-    conf.mongo.server + ':' + conf.mongo.port + '/' + conf.mongo.db, function(err, database) {
-    if (err) return console.log(err);
-    db = database;
+//mongo.connect('mongodb://' + conf.mongo.user + ':' + conf.mongo.pwd + '@'+
+//    conf.mongo.server + ':' + conf.mongo.port + '/' + conf.mongo.db, function(err, database) {
+//    if (err) return console.log(err);
+//    db = database;
 
     // Start server listening
     app.listen(conf.core.port, function(){
         console.log(conf.core.desc + ' is listening on port ' + conf.core.port);
     });
-});
+//});
 
 
 /********************************************************/
 /************** Facebook Messenger API ******************/
 /********************************************************/
-var fbm = fork_fbm('./fbm/app.js');
-fbm.on('exit', function (code) {
-    console.log(conf.fbm.desc + 'exited with exit code ' + code);
-});
+child.push(fork('./fbm/app.js'));
 
 
 app.post('/user', function(req, res) {
