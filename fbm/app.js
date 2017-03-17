1f525d5bd7da3715ad6b52da2259e55207408512
João Azevedo <jazevedo@veniam.com>
2017-03-18 00:27:00 +0800

    restructure code, start roomba support

---
+++ fbm/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var parser = require('body-parser');
+var conf = require('configure');
+
+// Routes
+var fbm = require('./routes/fbm');
+
+/********************************************************/
+/**** Skyler FBM configuration and initialization *******/
+/********************************************************/
+var app = express();
+
+process.on('uncaughtException', function (err) {
+    console.log('app.uncaughtException()', 'err', true, true, null, err.stack || err.message)
+});
+
+app.disable('x-powered-by');
+app.disable('etag');
+
+app.use('/fbm', fbm);
+
+app.listen(conf.server_port, function(){
+    console.log(conf.fbm.desc + ' is listening on port ' + conf.fbm.port);
+});
--- index.js
+++ index.js
@@ -3,10 +3,22 @@ var request = require('request');
 var conf = require('configure');
 var parser = require('body-parser');
 var mongo = require('mongodb').MongoClient;
+
+/********************************************************/
+/****** Skyler configuration and initialization *********/
+/********************************************************/
+
 var app = express();
+var fork_fbm = require('child_process').fork;
 var db;
 
+process.on('uncaughtException', function(err) {
+    console.log('app.uncaughtException()', 'err', true, true, null, err.stack || err.message);
+});
+
 app.use(parser.json());
+app.disable('x-powered-by');
+app.disable('etag');
 
 mongo.connect('mongodb://' + conf.mongo.user + ':' + conf.mongo.pwd + '@'+
     conf.mongo.server + ':' + conf.mongo.port + '/' + conf.mongo.db, function(err, database) {
@@ -15,66 +27,24 @@ mongo.connect('mongodb://' + conf.mongo.user + ':' + conf.mongo.pwd + '@'+
 
     // Start server listening
     app.listen(conf.server_port, function(){
-        console.log('Butler is listening on port ' + conf.server_port);
+        console.log(conf.core.desc + ' is listening on port ' + conf.core.port);
     });
 });
 
 
-// FB Messenger Webhook test
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === conf.fb.verify_token) {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }  
+/********************************************************/
+/************** Facebook Messenger API ******************/
+/********************************************************/
+var fbm = fork_fbm('./fbm/app.js');
+fbm.on('exit', function (code) {
+    console.log(conf.fbm.desc + 'exited with exit code ' + code);
 });
-
-// FB Messenger webhook
-app.post('/webhook', function (req, res) {
-  var data = req.body;
-
-  console.log("***Data:\n"+data.object);
-
-  // Make sure this is a page subscription
-  if (data.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
-    data.entry.forEach(function(entry) {
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
-
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.message) {
-          receivedMessage(event);
-        } else {
-          console.log("Webhook received unknown event: ", event);
-        }
-      });
-    });
-
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know
-    // you've successfully received the callback. Otherwise, the request
-    // will time out and we will keep trying to resend.
-    res.sendStatus(200);
-  }
-});
-  
-function receivedMessage(event) {
-  // Putting a stub for now, we'll expand it in the following steps
-  console.log("Message data: ", event.message);
-}
 
 
 app.post('/user', function(req, res) {
     if (!req.body) return res.sendStatus(400);
     else {
-        console.log("POST user:");
+        console.log('POST user:');
         console.log(req.body);
         res.sendStatus(200);
     }
@@ -83,5 +53,5 @@ app.post('/user', function(req, res) {
 app.get('*', function(req, res) {
     console.log(req.path);
     res.status(404);
-    res.send(req.path);
+    res.send('404 - Not Found');
 });
